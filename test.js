8f2f9b2c45bcd32a5bc8006b22bb3620bc26c6d7
Marcin Pietkiewicz <50468486+MarcinPietkiewicz@users.noreply.github.com>
2019-05-16 02:03:30 +0200

    added delayed notification

--- test.js
+++ test.js
@@ -1,28 +1,9 @@
+var myMessage = document.getElementById("message");
 
-function setUpEvents(){
-    var content = document.getElementById("content");
-    var button = document.getElementById("show-more");
-    
-    button.onclick = function(){
-        if (content.className == "open"){
-            // shrink the box
-            content.className="";
-            button.innerHTML = "Show More";
-        } else {
-            //expand the box
-            content.className="open";
-            button.innerHTML = "Show Less";
-        }
-    };
-};
+function showMessage(){
 
+    myMessage.className = "show"
 
+}
 
-window.onload = function(){
-setUpEvents();
-
-};
-
-
-
-
+setTimeout(showMessage, 3000);
